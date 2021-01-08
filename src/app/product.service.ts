d1562b65bbb4fde36f0f23bd4cd2d56838723f90
Michael Lin <michaellin@Cancans-MacBook-Pro.local>
2021-01-08 14:59:15 -0500

    update cli

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-
 @Injectable()
 export class ProductService {
 
@@ -11,7 +10,7 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => 
-    response.json());
+    return this._http.get(this._albumUrl).map(response => response.json());
   }
+
 }
